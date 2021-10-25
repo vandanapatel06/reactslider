a8c711bbaa271a282a5574f759d52e3635170a5c
vandanapatel06 <vandanapatel06.1993@gmail.com>
2021-10-25 15:30:33 -0700

    first commit

---
+++ src/components/SliderData.js
@@ -0,0 +1,26 @@
+export const SliderData = [
+    
+    {image: "https://founded.media/hiring/photos/dogs/photo-1477884213360-7e9d7dcc1e48.jpeg"},
+    {image: "https://founded.media/hiring/photos/dogs/photo-1534361960057-19889db9621e.jpeg"},
+    {image: "https://founded.media/hiring/photos/dogs/photo-1534551767192-78b8dd45b51b.jpeg"},
+    {image: "https://founded.media/hiring/photos/dogs/photo-1543466835-00a7907e9de1.jpeg"},
+    {image: "https://founded.media/hiring/photos/dogs/photo-1548199973-03cce0bbc87b.jpeg"},
+    {image: "https://founded.media/hiring/photos/dogs/photo-1561037404-61cd46aa615b.jpeg"},
+    {image: "https://founded.media/hiring/photos/dogs/photo-1576201836106-db1758fd1c97.jpeg"},
+    {image: "https://founded.media/hiring/photos/dogs/photo-1581888227599-779811939961.jpeg"},
+    {image: "https://founded.media/hiring/photos/dogs/photo-1583511655857-d19b40a7a54e.jpeg"},
+    {image: "https://founded.media/hiring/photos/dogs/photo-1587300003388-59208cc962cb.jpeg"},
+    {image: 'https://founded.media/hiring/photos/dinosaurs/photo-1519568262558-dc4b87dd85ca.jpeg'},
+    {image: 'https://founded.media/hiring/photos/dinosaurs/photo-1525877442103-5ddb2089b2bb.jpeg'},
+    {image: 'https://founded.media/hiring/photos/dinosaurs/photo-1559999127-b8b7f927dab8.jpeg'},
+    {image: 'https://founded.media/hiring/photos/dinosaurs/photo-1560148271-00b5e5850812.jpeg'},
+    {image: 'https://founded.media/hiring/photos/dinosaurs/photo-1568887786489-0662e7f51aab.jpeg'},
+    {image: 'https://founded.media/hiring/photos/dinosaurs/photo-1570482606740-a0b0baa0e58d.jpeg'},
+    {image: 'https://founded.media/hiring/photos/dinosaurs/photo-1579197073550-bf44b469a6fe.jpeg'},
+    {image: 'https://founded.media/hiring/photos/dinosaurs/photo-1583307359900-dbefeb18e3cc.jpeg'},
+    {image:'https://founded.media/hiring/photos/dinosaurs/photo-1583307709855-88a955597645.jpeg'},
+    {image: 'https://founded.media/hiring/photos/dinosaurs/photo-1606856110002-d0991ce78250.jpeg'},
+    
+
+   
+]
